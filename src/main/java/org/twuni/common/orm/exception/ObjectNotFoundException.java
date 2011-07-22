40eb8b692e8dd685adedb7ac64c015d671bb7a97
Leo Accend <github.aeonic@spamgourmet.com>
2011-07-22 14:34:54 -0700

    Adding a parameter to ObjectNotFoundException to help further troubleshoot why an object could not be found.

--- src/main/java/org/twuni/common/orm/exception/ObjectNotFoundException.java
+++ src/main/java/org/twuni/common/orm/exception/ObjectNotFoundException.java
@@ -1,5 +1,9 @@
 package org.twuni.common.orm.exception;
 
 public class ObjectNotFoundException extends RuntimeException {
+	
+	public ObjectNotFoundException( Object object ) {
+		super( String.format( "Object not found: %s", object ) );
+	}
 
 }
