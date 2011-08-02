437433bf249f65c980bb36220c0780c25158e55f
Leo Accend <github.aeonic@spamgourmet.com>
2011-08-01 18:21:57 -0700

    Adding Parameters.NONE to assist in reusing Session logic.

--- src/main/java/org/twuni/common/persistence/Parameters.java
+++ src/main/java/org/twuni/common/persistence/Parameters.java
@@ -2,6 +2,14 @@ package org.twuni.common.persistence;
 
 public interface Parameters {
 
+	public static final Parameters NONE = new Parameters() {
+
+		@Override
+		public void apply( Parameterized target ) {
+		}
+
+	};
+
 	public void apply( Parameterized target );
 
 }
